deccff95cf218a15b3c3ae09f261f29437edbc44
Jason Hooper <jason.hooper@talk21.com>
2014-02-21 16:47:10 +0000

    complete test for adding 3 points

--- spec/ruby_kml_herd_spec.rb
+++ spec/ruby_kml_herd_spec.rb
@@ -1,10 +1,20 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "RubyKmlHerd" do
-  it "adds a placemark to kml" do
-    binding.pry
-    kh = KMLHerd.new
-    kh.add_placemark('a', 1, 1, 'test')
-    kh.kml.should include('<name>a</name>')
+  before do
+    @kh = KMLHerd.new
+    @kh.add_placemark('a', 1, 1, 'test')
+    @kh.add_placemark('b', 2, 2, 'test')
+    @kh.add_placemark('c', 10, 10, 'test')
   end
+
+  it "adds placemarks to kml" do
+    @kh.kml.should include('<name>a</name>')
+    @kh.kml.should include('<coordinates>1,1</coordinates>')
+    @kh.kml.should include('<name>b</name>')
+    @kh.kml.should include('<coordinates>2,2</coordinates>')
+    @kh.kml.should include('<name>c</name>')
+    @kh.kml.should include('<coordinates>10,10</coordinates>')
+  end
+
 end
